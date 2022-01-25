fc6955f1d5a3ef7e3b4b2a85be95ee24f20c3506
MityanPala <NoEmailForYou>
2022-01-25 05:45:06 +0300

    little fix in includes - fixed file name

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 #include <stdlib.h>
 
 t_tree	*gc_create_node(int fd)
